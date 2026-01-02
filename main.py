1dd34b6e37d074932cd0ee63c0a46ca7deb8d15c
Menes Yuliia <yukamenes@gmail.com>
2026-01-02 14:05:55 +0200

    Added snake movement by arrows

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from turtle import Screen, Turtle
 import time
 from snake import Snake
 
-screen = Screen()
-screen.setup(600, 600)
+screen = Screen() 
+screen.setup(600, 600) 
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
+screen.listen()
 
 snake = Snake()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
  
 game_is_on = True
 
@@ -16,15 +21,5 @@ while game_is_on:
     snake.move()
     time.sleep(0.1) 
     screen.update()
-
-
-
-
-
-
-
-
-
-
 
 screen.exitonclick()
